f7501f72e0b2a1c58a295f4fe7c5585f10be0da0
David Alvarez <davidtcc@gmail.com>
2020-08-15 10:38:57 +0200

    gltf reader

---
+++ source/GLTFLoader.cpp
@@ -0,0 +1,42 @@
+#include "../include/GLTFLoader.h"
+#include <iostream>
+
+cgltf_data* LoadGLTFFile(const char* path) {
+    cgltf_options options;
+    memset(&options, 0, sizeof(cgltf_options));
+    cgltf_data* data = NULL;
+    
+    cgltf_result result = cgltf_parse_file(&options, path, &data);
+    if (result != cgltf_result_success) {
+        std::cout << "Could not load: " << path << std::endl;
+        return 0;
+    }
+
+    result = cgltf_load_buffers(&options, data, path);
+    if (result != cgltf_result_success) {
+        cgltf_free(data);
+        std::cout << "Could not load: " << path << std::endl;
+        return 0;
+    }
+
+    result = cgltf_validate(data);
+    if (result != cgltf_result_success) {
+        cgltf_free(data);
+        std::cout << "Invalid file: " << path << std::endl;
+        return 0;
+    }
+
+
+    return data;
+}
+
+void FreeGLTFFile(cgltf_data* handle) {
+    if (handle == 0) {
+        std::cout << "WARNING: Can't free null data \n";
+    }
+    else {
+        cgltf_free(handle);
+    }
+}
+
+
